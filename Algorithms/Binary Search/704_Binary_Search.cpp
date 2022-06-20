cf48f90d815e38715177af4c4356a9e17720ec47
Prabir Tarafdar <ptarafdar010@gmail.com>
2022-06-20 11:55:31 +0530

    Binary Search

---
+++ Algorithms/Binary Search/704_Binary_Search.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+    int search(vector<int>& nums, int key) {
+        int l = 0;
+        int r = nums.size()-1;
+        int mid = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums.size()==1){
+                if(nums[0]==key)
+                    return 0;
+                else
+                    return -1;
+            }
+            if(l > r){
+                return -1;
+            }
+            else{
+                mid = (l+r)/2;
+                if(nums[mid] > key){
+                    r = mid - 1;
+                }
+                if(nums[mid] == key){
+                    break;
+                }
+                if(nums[mid] < key){
+                    l = mid + 1;
+                }
+            }
+        }
+        return mid;
+    }
+
+int main(){
+    vector<int>arr = {-1,0,3,5,9,12};
+    int key = 9;
+    cout<<search(arr, key)<<endl;
+
+    return 0;
+}
